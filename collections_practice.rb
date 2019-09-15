fc4adafb9bd5fd6c8877d4170e8c81f2655c51b9
Cody Fussell <cody.fussell@gmail.com>
2019-09-15 20:41:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,14 @@ def sort_array_asc(list)
   return list.sort()
 end
 
-def sort_array_desc(list)
-  return
-  
+def sort__array_desc(list)
+    list.sort{|a,b|
+        if a == b
+            0
+        elsif a < b
+            1
+        elsif a > b
+            -1
+        end             
+    }
+end 
