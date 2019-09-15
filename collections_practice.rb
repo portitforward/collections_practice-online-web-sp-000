3f264fd3be9455dc14550cdc42fbd7c361ff64cc
Cody Fussell <cody.fussell@gmail.com>
2019-09-15 20:46:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,4 +15,13 @@ def sort_array_desc(list)
 end
 
 def sort_array_char_count(list)
-  
+    list.sort{|a,b|
+        if a.length == b.length
+            0
+        elsif a.length < b.length
+            1
+        elsif a.length > b.length
+            -1
+        end             
+    }
+end
